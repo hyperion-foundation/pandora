663fc65afe21c4c7cad6005d8643d9b022358466
Alexander Andhika S <31282709+sleepnov4@users.noreply.github.com>
2020-04-27 21:48:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
+const color = require("./color.json");
 const client = new Discord.Client();
 
 client.on("ready", async () => {
-    console.log(`${client.user.username} is ready to go!`);
-    client.user.setActivity(`Hyperion Discord server`, {type: "WATCHING"});
+    console.log(`${client.user.username} is online.`);
+    client.user.setActivity("Developed By Hyperion Dev Team", {type: "CUSTOM_STATUS"});
 });
 
-client.on(`guildMemberAdd`, member => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome-goodbye');
+client.on(`guildMemberAdd`, memberadd => {
+    const channel = memberadd.guild.channels.cache.find(ch => ch.name === 'welcome-goodbye');
     if(!channel) return;
-    channel.send(`Welcome, ${member}! Enjoy your stay and have fun.`);
+    channel.send(`Welcome, ${memberadd} ! Enjoy your stay and have fun.`);
 });
 
-client.on(`guildMemberRemove`, member => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome-goodbye');
+client.on(`guildMemberRemove`, memberremove => {
+    const channel = memberremove.guild.channels.cache.find(ch => ch.name === 'welcome-goodbye');
     if(!channel) return;
-    channel.send(`Goodbye, ${member}. We are sorry to see you go.`);
+    channel.send(`Goodbye, ${memberremove} . We are sorry to see you go.`);
+});
+
+client.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = config.prefix;
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(command === `${prefix}hi`) {
+        return message.channel.send("Hi! Nice to see you. :wave:");
+    }
 });
 
 client.login(config.token);
