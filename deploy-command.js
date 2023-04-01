bf48148226513451f74097bce6067e69af375acf
Alexander Andhika Suryaperdana <frostgamers.fx@gmail.com>
2023-04-01 17:14:43 +0700

    Remove unused codes

--- deploy-command.js
+++ deploy-command.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const { REST, Routes } = require('discord.js');
-// const { clientId, guildId } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
